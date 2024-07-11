c97dcab2679168a0520d16b1b31783c83d252342
Raphael Maschinsen <raphaelmijnssen1@yahoo.de>
2024-07-11 23:28:43 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,12 +2,13 @@ import os
 import evdev
 from evdev import InputDevice, categorize, ecodes
 import subprocess
+import threading
 
 # Path to audio files
 audio_files = {
     'KEY_A': 'File1.mp3',
     'KEY_B': 'File2.mp3',
-    'KEY_Y': 'File2.mp3',
+    'KEY_Y': 'File3.mp3',  # Assuming this was intended to be File3.mp3
 }
 
 # Path to the input devices (footswitches)
@@ -22,14 +23,29 @@ def play_audio(file_path):
     # Play the new audio file
     subprocess.run(['mpg123', file_path])
 
-print(f'Listening for key presses on {device_paths}...')
-
-for device in devices:
+def listen_device(device):
+    print(f'Start listening on {device.path}')
     for event in device.read_loop():
         if event.type == ecodes.EV_KEY:
             key_event = categorize(event)
             if key_event.keystate == key_event.key_down:
                 key_code = key_event.keycode
                 if key_code in audio_files:
-                    print(f'Key {key_code} pressed, playing {audio_files[key_code]}')
+                    print(f'Key {key_code} pressed on {device.path}, playing {audio_files[key_code]}')
                     play_audio(audio_files[key_code])
+
+# Create and start a thread for each device
+threads = []
+for device in devices:
+    thread = threading.Thread(target=listen_device, args=(device,))
+    thread.daemon = True
+    thread.start()
+    threads.append(thread)
+
+# Keep the main thread running to allow the listening threads to operate
+print(f'Listening for key presses on {device_paths}...')
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Stopping...")
