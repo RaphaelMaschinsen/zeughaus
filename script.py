dd1670c49aab7f3844b014d0f6ba29d41b59a655
Raphael Maschinsen <raphaelmijnssen1@yahoo.de>
2024-07-30 16:17:22 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,76 +1,39 @@
-import os
-import evdev
-from evdev import InputDevice, categorize, ecodes
 import subprocess
 import threading
 import signal
 import sys
 
-audio_files = {
-    'KEY_A': 'File1.wav',
-    'KEY_B': 'File2.wav',
-    'KEY_Y': 'File3.wav',
-}
-
-device_paths = ['/dev/footswitch1', '/dev/footswitch2', '/dev/footswitch3']
-
-devices = [InputDevice(path) for path in device_paths]
-audio_device = 'hw:0,0'
+audio_file = 'File1.wav'  # The file to play on loop
 
 running = True
-playback_lock = threading.Lock()
 current_process = None
 
-def play_audio(file_path):
+def play_audio_loop(file_path):
     global current_process
-    with playback_lock:
-        if current_process:
-            current_process.terminate()
-            current_process.wait()
+    while running:
         current_process = subprocess.Popen(['aplay', file_path])
-
-def monitor_device(device):
-    print(f'Start monitoring {device.path}')
-    device.grab()  # Grab the device to get exclusive access
-    try:
-        while running:
-            for event in device.read_loop():
-                if not running:
-                    break
-                if event.type == ecodes.EV_KEY:
-                    key_event = categorize(event)
-                    if key_event.keystate == key_event.key_down:
-                        key_code = key_event.keycode
-                        if key_code in audio_files:
-                            print(f'Key {key_code} pressed on {device.path}, playing {audio_files[key_code]}')
-                            play_audio(audio_files[key_code])
-                            # Break the loop to handle the next input immediately
-                            break
-    finally:
-        device.ungrab()
+        current_process.wait()
 
 def signal_handler(sig, frame):
     global running
     print('Exiting gracefully...')
     running = False
-    for device in devices:
-        device.close()
+    if current_process:
+        current_process.terminate()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-print(f'Listening for key presses on {device_paths}...')
+print(f'Starting to play {audio_file} on loop...')
 
-threads = []
-for device in devices:
-    thread = threading.Thread(target=monitor_device, args=(device,))
-    threads.append(thread)
-    thread.start()
+# Start the audio loop in a separate thread
+thread = threading.Thread(target=play_audio_loop, args=(audio_file,))
+thread.start()
 
 # Keep the main thread alive
 while running:
     signal.pause()
 
-for device in devices:
-    device.close()
+if current_process:
+    current_process.terminate()
